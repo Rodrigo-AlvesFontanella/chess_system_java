07866f6b091521eb9a5b98f8b381db882b9163ef
Rodrigo Alves Fontanella <fontanellarodrigo13@gmail.com>
2022-04-28 20:06:19 -0300

    moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,7 +1,5 @@
 package boardgame;
-
 public class Board {
-
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
@@ -10,26 +8,15 @@ public class Board {
 		if (rows < 1 || columns < 1) {
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
-		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
-
 	public int getRows() {
 		return rows;
 	}
-
 	public int getColumns() {
 		return columns;
-	}
-
-	public Piece[][] getPieces() {
-		return pieces;
-	}
-
-	public void setPieces(Piece[][] pieces) {
-		this.pieces = pieces;
 	}
 	
 	public Piece piece(int row, int column) {
@@ -38,7 +25,7 @@ public class Board {
 		}
 		return pieces[row][column];
 	}
-
+	
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
@@ -53,14 +40,28 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
-
+	
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
-
+	
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
@@ -68,5 +69,3 @@ public class Board {
 		return piece(position) != null;
 	}
 }
-
-
