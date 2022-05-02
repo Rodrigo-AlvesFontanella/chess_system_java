094acfaa33be2adddbbbac9e5a7d9082d84e1645
Rodrigo Alves Fontanella <fontanellarodrigo13@gmail.com>
2022-05-02 18:41:32 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,8 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
+
 			try {
 				UI.clearScreen();
 
@@ -47,5 +48,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
